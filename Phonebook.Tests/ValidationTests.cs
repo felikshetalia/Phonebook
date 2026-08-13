08cc0eeeac3005cee8686746e8c0fb73602273e7
felikshetalia <mccorsica@yahoo.com>
2026-08-13 10:24:11 +0200

    switch to parametrized tests cases

--- Phonebook.Tests/ValidationTests.cs
+++ Phonebook.Tests/ValidationTests.cs
@@ -55,52 +55,37 @@
             });
         }
 
-        [Test]
-        public void IsContactIdValid_ReturnsFalseForInvalidOrNegativeId()
+        [TestCase("0")]
+        [TestCase("-5")]
+        [TestCase("abc")]
+        public void IsContactIdValid_ReturnsFalseForInvalidOrNegativeId(string input)
         {
-            bool result1 = Validators.IsContactIdValid("0", out int id1);
-            bool result2 = Validators.IsContactIdValid("-5", out int id2);
-            bool result3 = Validators.IsContactIdValid("abc", out int id3);
+            bool result = Validators.IsContactIdValid(input, out int id);
 
-            Assert.Multiple(() =>
-            {
-                Assert.That(result1, Is.False);
-                Assert.That(result2, Is.False);
-                Assert.That(result3, Is.False);
-            });
+            Assert.That(result, Is.False);
         }
 
-        [Test]
-        public void IsEmailValid_ReturnsTrueForValidEmailStrings()
+        [TestCase("user@example.com", true)]
+        [TestCase("user.name+tag@example.co.uk", true)]
+        [TestCase("", false)]
+        [TestCase("   ", false)]
+        [TestCase("invalid-email", false)]
+        [TestCase("user@localhost", false)]
+        public void IsEmailValid_ReturnsCorrectResult(string email, bool expected)
         {
-            Assert.That(Validators.IsEmailValid("user@example.com"), Is.True);
-            Assert.That(Validators.IsEmailValid("user.name+tag@example.co.uk"), Is.True);
+            Assert.That(Validators.IsEmailValid(email), Is.EqualTo(expected));
         }
 
-        [Test]
-        public void IsEmailValid_ReturnsFalseForInvalidOrEmptyEmails()
+        [TestCase("+12345678901", true)]
+        [TestCase("1234567890", true)]
+        [TestCase("", false)]
+        [TestCase("   ", false)]
+        [TestCase("+0123456", false)]
+        [TestCase("phone123", false)]
+        [TestCase("+12345", false)]
+        public void IsPhoneNumberValid_ReturnsCorrectResult(string phoneNumber, bool expected)
         {
-            Assert.That(Validators.IsEmailValid(""), Is.False);
-            Assert.That(Validators.IsEmailValid("   "), Is.False);
-            Assert.That(Validators.IsEmailValid("invalid-email"), Is.False);
-            Assert.That(Validators.IsEmailValid("user@localhost"), Is.False);
-        }
-
-        [Test]
-        public void IsPhoneNumberValid_ReturnsTrueForValidPhoneNumbers()
-        {
-            Assert.That(Validators.IsPhoneNumberValid("+12345678901"), Is.True);
-            Assert.That(Validators.IsPhoneNumberValid("1234567890"), Is.True);
-        }
-
-        [Test]
-        public void IsPhoneNumberValid_ReturnsFalseForInvalidOrEmptyPhones()
-        {
-            Assert.That(Validators.IsPhoneNumberValid(""), Is.False);
-            Assert.That(Validators.IsPhoneNumberValid("   "), Is.False);
-            Assert.That(Validators.IsPhoneNumberValid("+0123456"), Is.False);
-            Assert.That(Validators.IsPhoneNumberValid("phone123"), Is.False);
-            Assert.That(Validators.IsPhoneNumberValid("+12345"), Is.False);
+            Assert.That(Validators.IsPhoneNumberValid(phoneNumber), Is.EqualTo(expected));
         }
     }
 }
